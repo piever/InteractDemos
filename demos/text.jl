cd8d5c9ad91caeee5a2bd09f9ac828d185e38965
Pietro Vertechi <pietro.vertechi@neuro.fchampalimaud.org>
2018-07-19 16:21:14 +0100

    add julia source

--- demos/text.jl
+++ demos/text.jl
@@ -7,6 +7,24 @@
     @layout! wdg Widgets.div(:text, :textarea, :number, _.display)
 end
 
+code = Node(
+    :pre,
+    Node(
+        :code,
+        """
+        @widget wdg function textinputdemo()
+            :text = textbox(value = "Initial text", label = "This is a simple text box")
+            :textarea = textarea(placeholder = "Here you can write several lines", label = "A larger text box")
+            :number = spinbox(value = 0, label = "Here you can put numbers")
+            @output! wdg string("Here you wrote ", \$(:text), " and ", \$(:textarea), " and your number is ", \$(:number))
+            @display! wdg InteractBase.center(\$(_.output))
+            @layout! wdg Widgets.div(:text, :textarea, :number, _.display)
+        end
+        """,
+        className = "language-julia"
+    )
+)
+
 fct = function (req)
     Widgets.div(
         WebIO.render(
@@ -29,7 +47,7 @@ fct = function (req)
                 className = "section"
             ),
             Widgets.div(
-                togglecontent("Test!", label = "Show code"),
+                togglecontent(code, label = "Show code"),
                 className = "section has-background-light"
             ),
             className = "container"
