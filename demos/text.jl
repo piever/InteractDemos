f3ebc9571f4bf089d779041ad6d28a470090a97d
Pietro Vertechi <pietro.vertechi@neuro.fchampalimaud.org>
2018-07-19 16:36:53 +0100

    Added syntax highlight

--- demos/text.jl
+++ demos/text.jl
@@ -8,22 +8,34 @@
 end
 
 code = Node(
-    :pre,
+    :div,
     Node(
-        :code,
-        """
-        @widget wdg function textinputdemo()
-            :text = textbox(value = "Initial text", label = "This is a simple text box")
-            :textarea = textarea(placeholder = "Here you can write several lines", label = "A larger text box")
-            :number = spinbox(value = 0, label = "Here you can put numbers")
-            @output! wdg string("Here you wrote ", \$(:text), " and ", \$(:textarea), " and your number is ", \$(:number))
-            @display! wdg InteractBase.center(\$(_.output))
-            @layout! wdg Widgets.div(:text, :textarea, :number, _.display)
-        end
-        """,
-        className = "language-julia"
-    )
+        :pre,
+        Node(
+            :code,
+            """
+            @widget wdg function textinputdemo()
+                :text = textbox(value = "Initial text", label = "This is a simple text box")
+                :textarea = textarea(placeholder = "Here you can write several lines", label = "A larger text box")
+                :number = spinbox(value = 0, label = "Here you can put numbers")
+                @output! wdg string("Here you wrote ", \$(:text), " and ", \$(:textarea), " and your number is ", \$(:number))
+                @display! wdg InteractBase.center(\$(_.output))
+                @layout! wdg Widgets.div(:text, :textarea, :number, _.display)
+            end
+            """,
+            className = "language-julia"
+        )
+    ),
+    className = "content"
 )
+
+scp = Scope(imports = [
+    joinpath.(@__DIR__, "..", "css", "style.css"),
+    joinpath.(@__DIR__, "..", "highlight", "prism.css"),
+    joinpath.(@__DIR__, "..", "highlight", "prism.js")
+])
+
+scp.dom = code
 
 fct = function (req)
     Widgets.div(
@@ -47,7 +59,7 @@ fct = function (req)
                 className = "section"
             ),
             Widgets.div(
-                togglecontent(code, label = "Show code"),
+                togglecontent(scp, label = "Show code"),
                 className = "section has-background-light"
             ),
             className = "container"
