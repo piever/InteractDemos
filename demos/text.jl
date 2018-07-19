4edc184f121765068c354b3b2b9ae406079974c5
Pietro Vertechi <pietro.vertechi@neuro.fchampalimaud.org>
2018-07-19 17:34:23 +0100

    refactor

--- demos/text.jl
+++ demos/text.jl
@@ -7,35 +7,17 @@
     @layout! wdg Widgets.div(:text, :textarea, :number, _.display)
 end
 
-code = Node(
-    :div,
-    Node(
-        :pre,
-        Node(
-            :code,
-            """
-            @widget wdg function textinputdemo()
-                :text = textbox(value = "Initial text", label = "This is a simple text box")
-                :textarea = textarea(placeholder = "Here you can write several lines", label = "A larger text box")
-                :number = spinbox(value = 0, label = "Here you can put numbers")
-                @output! wdg string("Here you wrote ", \$(:text), " and ", \$(:textarea), " and your number is ", \$(:number))
-                @display! wdg InteractBase.center(\$(_.output))
-                @layout! wdg Widgets.div(:text, :textarea, :number, _.display)
-            end
-            """,
-            className = "language-julia"
-        )
-    ),
-    className = "content"
-)
-
-scp = Scope(imports = [
-    joinpath.(@__DIR__, "..", "css", "style.css"),
-    joinpath.(@__DIR__, "..", "highlight", "prism.css"),
-    joinpath.(@__DIR__, "..", "highlight", "prism.js")
-])
-
-scp.dom = code
+code =
+    """
+    @widget wdg function textinputdemo()
+        :text = textbox(value = "Initial text", label = "This is a simple text box")
+        :textarea = textarea(placeholder = "Here you can write several lines", label = "A larger text box")
+        :number = spinbox(value = 0, label = "Here you can put numbers")
+        @output! wdg string("Here you wrote ", \$(:text), " and ", \$(:textarea), " and your number is ", \$(:number))
+        @display! wdg InteractBase.center(\$(_.output))
+        @layout! wdg Widgets.div(:text, :textarea, :number, _.display)
+    end
+    """
 
 fct = function (req)
     Widgets.div(
@@ -46,7 +28,7 @@ fct = function (req)
                 className = "section"
             ),
             Widgets.div(
-                togglecontent(scp, label = "Show code"),
+                togglecontent(highlight(code), label = "Show code"),
                 className = "section has-background-light"
             ),
             className = "container"
--- utils.jl
+++ utils.jl
@@ -31,3 +31,27 @@ header(title) =
             """
         )
     )
+
+function highlight(code)
+    codeblock = Node(
+        :div,
+        Node(
+            :pre,
+            Node(
+                :code,
+                code,
+                className = "language-julia"
+            )
+        ),
+        className = "content"
+    )
+
+    scp = Scope(imports = [
+        joinpath.(@__DIR__, "css", "style.css"),
+        joinpath.(@__DIR__, "highlight", "prism.css"),
+        joinpath.(@__DIR__, "highlight", "prism.js")
+    ])
+
+    scp.dom = codeblock
+    scp
+end
