5f9be1cc39fd72d012ebb04323f6f4f263270e05
Pietro Vertechi <pietro.vertechi@neuro.fchampalimaud.org>
2019-05-14 12:33:31 +0100

    restore

--- app.jl
+++ app.jl
@@ -3,7 +3,7 @@ const HOST_URL = "0.0.0.0"
 
 ENV["WEBIO_SERVER_HOST_URL"] = HOST_URL
 ENV["WEBIO_HTTP_PORT"] = PORT
-ENV["JULIA_WEBIO_BASEURL"] = "https://interactdemos.herokuapp.com/"
-ENV["WEBIO_WEBSOCKT_URL"] = string(HOST_URL, ":", PORT, "/webio_websocket/")
+# ENV["JULIA_WEBIO_BASEURL"] = "https://interactdemos.herokuapp.com/"
+# ENV["WEBIO_WEBSOCKT_URL"] = string(HOST_URL, ":", PORT, "/webio_websocket/")
 
 include("main.jl")
