e0a622833c642e07058e4f68f262eb04469a3db8
Pietro Vertechi <pietro.vertechi@neuro.fchampalimaud.org>
2019-05-14 12:28:54 +0100

    second try

--- app.jl
+++ app.jl
@@ -1,7 +1,9 @@
-ENV["WEBIO_SERVER_HOST_URL"] = "0.0.0.0"
-ENV["WEBIO_HTTP_PORT"] = isempty(ARGS) ? 8888 : parse(Int64, ARGS[1])
+const PORT = isempty(ARGS) ? 8888 : parse(Int64, ARGS[1])
+const HOST_URL = "0.0.0.0"
 
+ENV["WEBIO_SERVER_HOST_URL"] = HOST_URL
+ENV["WEBIO_HTTP_PORT"] = PORT
 ENV["JULIA_WEBIO_BASEURL"] = "https://interactdemos.herokuapp.com/"
-ENV["WEBIO_WEBSOCKT_URL"] = "ws://interactdemos.herokuapp.com/webio_websocket/"
+ENV["WEBIO_WEBSOCKT_URL"] = string(HOST_URL, ":", PORT, "/webio_websocket/")
 
 include("main.jl")
